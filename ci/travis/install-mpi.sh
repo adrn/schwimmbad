23a62df3ba4540c8c102d621a4c6e90db73501a3
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-09-16 12:32:20 -0400

    try installing mpi and mpi4py

---
+++ ci/travis/install-mpi.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+case `uname` in
+Linux)
+  sudo apt-get update -q
+  case $1 in
+    mpich) set -x;
+      sudo apt-get install -y -q mpich libmpich-dev
+      ;;
+    openmpi) set -x;
+      sudo apt-get install -y -q openmpi-bin libopenmpi-dev
+      ;;
+    *)
+      echo "Unknown MPI implementation:" $1
+      exit 1
+      ;;
+  esac
+  ;;
