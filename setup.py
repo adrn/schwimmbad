177f69163118283f133673ffe3624513035f8288
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-09-16 11:06:52 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name="schwimmbad",
+    version='v0.1',
+    author="Adrian Price-Whelan",
+    author_email="adrn@princeton.edu",
+    packages=["schwimmbad"],
+    url="https://github.com/adrn/schwimmbad",
+    license="MIT",
+    description="A common interface for multiprocessing pools.",
+    long_description="",
+    package_data={"": ["LICENSE"]},
+    include_package_data=True,
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ],
+)
