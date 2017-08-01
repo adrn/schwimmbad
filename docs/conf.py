21d90a838c7b43737d98e345bc49a26077ae4587
Adrian Price-Whelan <adrian.prw@gmail.com>
2017-08-01 21:14:55 +0200

    use sphinx rtd

--- docs/conf.py
+++ docs/conf.py
@@ -84,9 +84,6 @@ if not on_rtd:
     html_theme = "sphinx_rtd_theme"
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
-else:
-    html_theme = 'alabaster'
-
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
