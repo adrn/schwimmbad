f95f55ba019266e81074ad1a02d5f02328c6d484
Adrian Price-Whelan <adrian.prw@gmail.com>
2017-08-08 08:10:09 +0200

    don't check commworld

--- schwimmbad/tests/test_mpi.py
+++ schwimmbad/tests/test_mpi.py
@@ -19,8 +19,6 @@ def _callback(x):
 
 @pytest.mark.skip(True, reason="WTF")
 def test_mpi():
-    assert MPI.COMM_WORLD.Get_rank() == 0
-
     pool = MPIPool()
 
     pool.wait(lambda: sys.exit(0))
@@ -33,11 +31,15 @@ def test_mpi():
         for r1,r2 in zip(results, [_function(x) for x in tasks]):
             assert isclose(r1, r2)
 
+        assert len(results) == len(tasks)
+
     # test map with callback
     for tasks in all_tasks:
         results = pool.map(_function, tasks, callback=_callback)
         for r1,r2 in zip(results, [_function(x) for x in tasks]):
             assert isclose(r1, r2)
+
+        assert len(results) == len(tasks)
 
     pool.close()
 
