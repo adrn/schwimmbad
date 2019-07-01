f721a7e4e29cd09d25fbef5ed2c35fbca5c9ca88
Anowar Jaman Shajib <ajshajib@gmail.com>
2019-07-01 01:38:50 -0700

    Add option for dill serialization

--- schwimmbad/mpi.py
+++ schwimmbad/mpi.py
@@ -19,10 +19,10 @@ def _import_mpi(quiet=False, use_dill=False):
     global MPI
     try:
         import mpi4py.MPI
+        MPI = mpi4py.MPI
         if use_dill:
             import dill
             MPI.pickle.__init__(dill.dumps, dill.loads)
-        MPI = mpi4py.MPI
     except ImportError:
         if not quiet:
             # Re-raise with a more user-friendly error:
