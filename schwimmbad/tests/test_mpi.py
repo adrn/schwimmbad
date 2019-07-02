d2621b9ec955ebe8d87d180add792cdbbd2c6d88
Anowar Jaman Shajib <ajshajib@gmail.com>
2019-07-01 18:16:49 -0700

    Update MPIPool test with dill implementataion

--- schwimmbad/tests/test_mpi.py
+++ schwimmbad/tests/test_mpi.py
@@ -17,33 +17,51 @@ from schwimmbad.mpi import MPIPool, MPI
 def _callback(x):
     pass
 
-#@pytest.mark.skip(True, reason="WTF")
-def test_mpi_with_dill():
-    pool = MPIPool(use_dill=True)
+@pytest.mark.skip(True, reason="WTF")
+def test_mpi():
+    with MPIPool() as pool:
+        pool.wait(lambda: sys.exit(0))
 
-    pool.wait(lambda: sys.exit(0))
+        all_tasks = [[random.random() for i in range(1000)]]
 
-    all_tasks = [[random.random() for i in range(1000)]]
+        # test map alone
+        for tasks in all_tasks:
+            results = pool.map(_function, tasks)
+            for r1,r2 in zip(results, [_function(x) for x in tasks]):
+                assert isclose(r1, r2)
 
-    # test map alone
-    for tasks in all_tasks:
-        results = pool.map(_function, tasks)
-        for r1,r2 in zip(results, [_function(x) for x in tasks]):
-            assert isclose(r1, r2)
+            assert len(results) == len(tasks)
 
-        assert len(results) == len(tasks)
+        # test map with callback
+        for tasks in all_tasks:
+            results = pool.map(_function, tasks, callback=_callback)
+            for r1,r2 in zip(results, [_function(x) for x in tasks]):
+                assert isclose(r1, r2)
 
-    # test map with callback
-    for tasks in all_tasks:
-        results = pool.map(_function, tasks, callback=_callback)
-        for r1,r2 in zip(results, [_function(x) for x in tasks]):
-            assert isclose(r1, r2)
+            assert len(results) == len(tasks)
 
-        assert len(results) == len(tasks)
+    # test for dill serialization
+    with MPIPool(use_dill=True) as pool:
+        pool.wait(lambda: sys.exit(0))
 
-    pool.close()
+        all_tasks = [[random.random() for i in range(1000)]]
+
+        # test map alone
+        for tasks in all_tasks:
+            results = pool.map(_function, tasks)
+            for r1, r2 in zip(results, [_function(x) for x in tasks]):
+                assert isclose(r1, r2)
+
+            assert len(results) == len(tasks)
+
+        # test map with callback
+        for tasks in all_tasks:
+            results = pool.map(_function, tasks, callback=_callback)
+            for r1, r2 in zip(results, [_function(x) for x in tasks]):
+                assert isclose(r1, r2)
+
+            assert len(results) == len(tasks)
 
 
 if __name__ == '__main__':
-    #test_mpi()
-    test_mpi_with_dill()
+    test_mpi()
