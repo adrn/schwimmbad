943eea69f4773bca3d2e5f718e663a31e2f33825
Adrian Price-Whelan <adrian.prw@gmail.com>
2020-07-24 15:57:53 -0400

    add batched_map method for pools

--- schwimmbad/tests/test_pools.py
+++ schwimmbad/tests/test_pools.py
@@ -1,5 +1,4 @@
 # Standard library
-from __future__ import division, print_function, absolute_import, unicode_literals
 import time
 import random
 
@@ -8,8 +7,10 @@ from ..serial import SerialPool
 from ..multiprocessing import MultiPool
 from ..jl import JoblibPool
 
+
 def isclose(a, b, rel_tol=1e-09, abs_tol=0.0):
     return abs(a-b) <= max(rel_tol * max(abs(a), abs(b)), abs_tol)
+
 
 def _function(x):
     """
@@ -17,6 +18,18 @@ def _function(x):
     """
     time.sleep(random.random()*4E-4 + 1E-4)
     return 42.01
+
+
+def _batch_function(batch):
+    """
+    Wastes a random amount of time, returns the number 42.01 - accepts a batch
+    of tasks instead of a single task
+    """
+    results = []
+    for x in batch:
+        time.sleep(random.random()*4E-4 + 1E-4)
+        results.append(42.01)
+    return results
 
 
 class PoolTestBase(object):
